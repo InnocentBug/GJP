3edc5e646bb9476bdd140859c921ac7dee8f8e6a
Ludwig Schneider <ludwigschneider@uchicago.edu>
2024-06-11 09:48:37 -0500

    first draft for edge weight decoder

---
+++ gjp/edge_weight_gae.py
@@ -0,0 +1,106 @@
+from functools import partial
+from typing import Any, Sequence
+
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+
+from .edge_weight_decoder import EdgeWeightDecoder
+from .mpg_edge_weight import (
+    MessagePassingEW,
+    edge_weights_n_edge_loss,
+    edge_weights_sharpness_loss,
+)
+
+
+class EdgeWeightGAE(nn.Module):
+    max_num_nodes: int
+    encoder_stack: Sequence[Sequence[int]]
+    node_stack: Sequence[int]
+    edge_stack: Sequence[int]
+    decoder_mpg_stack: Sequence[Sequence[int]]
+    multi_edge_repeat: int = 1
+    mlp_kwargs: dict[str, Any] | None = None
+
+    def setup(self):
+        self._mlp_kwargs = self.mlp_kwargs
+        if self._mlp_kwargs is None:
+            self._mlp_kwargs = {}
+
+        self.encoder = MessagePassingEW(node_feature_sizes=self.encoder_stack, edge_feature_sizes=self.encoder_stack, global_feature_sizes=self.encoder_stack, mean_instead_of_sum=False, mlp_kwargs=self._mlp_kwargs)
+
+        self.sigma_encoder = MessagePassingEW(node_feature_sizes=self.encoder_stack, edge_feature_sizes=self.encoder_stack, global_feature_sizes=self.encoder_stack, mean_instead_of_sum=False, mlp_kwargs=self._mlp_kwargs)
+
+        self.decoder = EdgeWeightDecoder(max_nodes=self.max_num_nodes, init_node_stack=self.node_stack, init_edge_stack=self.edge_stack, prob_mpg_stack=self.decoder_mpg_stack, multi_edge_repeat=self.multi_edge_repeat, mlp_kwargs=self._mlp_kwargs)
+
+    def __call__(self, x):
+        mu_wo_noise = self.encoder(x).globals
+        log_sigma_wo_noise = self.sigma_encoder(x).globals
+        sigma_wo_noise = jnp.exp(log_sigma_wo_noise)
+        rng = self.make_rng("reparametrize")
+        eps = jax.random.normal(rng, mu_wo_noise.shape)
+        z_wo_node = mu_wo_noise + sigma_wo_noise * eps  # Reparameterization trick
+
+        # Add the node and edge info after adding noise
+        z = jnp.vstack((z_wo_node.transpose(), x.n_node, x.n_edge)).transpose()
+        reconstructed, edge_weights = self.decoder(z)
+        return reconstructed, edge_weights, mu_wo_noise, log_sigma_wo_noise
+
+    def decode(self, z):
+        return self.decoder(z)
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def encode_decode(self, x):
+        mu_wo_node = self.encoder(x).globals
+        # Add the node and edge info after adding noise
+        z = jnp.vstack((mu_wo_node.transpose(), x.n_node, x.n_edge)).transpose()
+        reconstructed, edge_weights = self.decoder(z)
+        return reconstructed, edge_weights
+
+
+def pre_loss_function(params, train_state, in_graphs, rngs, metric_state):
+    in_metric = metric_state.apply_fn(metric_state.params, in_graphs).globals[:-1]
+
+    tmp_out_graphs, edge_weights, mu, log_sigma = train_state.apply_fn(params, in_graphs, rngs=rngs)
+    embedded_space = tmp_out_graphs.globals[::2]
+    embedded_space = embedded_space[:, :-2]
+
+    out_globals = jnp.hstack([in_graphs.globals, in_graphs.globals * 0]).reshape(tmp_out_graphs.globals.shape[0], in_graphs.globals.shape[1])
+    out_graphs = tmp_out_graphs._replace(globals=out_globals)
+
+    out_metric = metric_state.apply_fn(metric_state.params, out_graphs, edge_weights).globals[::2]
+    out_metric = out_metric[:-1]
+
+    recon_loss = jnp.sqrt(jnp.mean((in_metric - out_metric) ** 2))
+    kl_divergence = -0.5 * jnp.sum(1 + log_sigma - jnp.square(mu) - jnp.exp(log_sigma))
+    sharp_loss = edge_weights_sharpness_loss(edge_weights)
+    n_edge_loss = edge_weights_n_edge_loss(edge_weights, out_graphs.n_edge[::2])
+
+    return recon_loss, kl_divergence, sharp_loss, n_edge_loss
+
+
+def loss_function(params, train_state, in_graphs, rngs, metric_state):
+    recon_loss, kl_divergence, sharp_loss, n_edge_loss = pre_loss_function(params, train_state, in_graphs, rngs, metric_state)
+    return recon_loss + kl_divergence + sharp_loss + n_edge_loss
+
+
+def train_step(batch_train, batch_test, train_state, rng, metric_state):
+    loss_fn = partial(loss_function, metric_state=metric_state)
+    pre_loss_fn = partial(pre_loss_function, metric_state=metric_state)
+
+    loss_grad_fn = jax.value_and_grad(loss_fn)
+
+    rng, rng_reparametrize_train, rng_reparametrize_test, rng_drop_train, rng_drop_test = jax.random.split(rng, 5)
+
+    rngs_train = {"reparametrize": rng_reparametrize_train, "dropout": rng_drop_train}
+    rngs_test = {"reparametrize": rng_reparametrize_test, "dropout": rng_drop_test}
+
+    train_recon, train_kl, train_sharp, train_n_edge = pre_loss_fn(train_state.params, train_state, batch_train, rngs_train)
+    test_recon, test_kl, test_sharp, test_n_edge = pre_loss_fn(train_state.params, train_state, batch_test, rngs_test)
+
+    train_loss, grads = loss_grad_fn(train_state.params, train_state, batch_train, rngs_train)
+    train_state = train_state.apply_gradients(grads=grads)
+
+    return train_state, train_loss, train_recon, train_kl, train_sharp, train_n_edge, test_recon, test_kl, test_sharp, test_n_edge
---
+++ tests/test_edge_weight_gae.py
@@ -0,0 +1,91 @@
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+import jraph
+import optax
+from flax.training.train_state import TrainState
+
+from gjp import bag_gae, edge_weight_decoder, edge_weight_gae, mpg_edge_weight
+
+
+def test_edge_weight_gae(batch_graphs, mlp_kwargs):
+    max_num_nodes = jnp.max(batch_graphs.n_node) + 1
+    multi_edge_repeat = bag_gae.find_multi_edge_repeat(batch_graphs)
+
+    mlp_stack = [1, 4, 32, 16, 4, 2]
+    mpg_stack = [[2], [4, 8], [16], [2]]
+
+    model = edge_weight_gae.EdgeWeightGAE(
+        max_num_nodes=max_num_nodes,
+        encoder_stack=mpg_stack,
+        node_stack=mlp_stack,
+        edge_stack=mlp_stack,
+        decoder_mpg_stack=mpg_stack,
+        multi_edge_repeat=multi_edge_repeat,
+        mlp_kwargs=mlp_kwargs,
+    )
+    rng = jax.random.key(234)
+    rng, rng_split_a, rng_split_b = jax.random.split(rng, 3)
+
+    params = model.init({"params": rng, "reparametrize": rng_split_a, "dropout": rng_split_b}, batch_graphs)
+    rng, rng_split_a, rng_split_b = jax.random.split(rng, 3)
+
+    apply_model = jax.jit(lambda x, y, z, w: model.apply(x, y, rngs={"reparametrize": z, "dropout": w}))
+    out_graphs, edge_weights, mu, sigma = apply_model(params, batch_graphs, rng_split_a, rng_split_b)
+    rng, rng_split = jax.random.split(rng)
+
+    out_graphs = edge_weight_decoder.make_graph_sparse(out_graphs, edge_weights)
+
+    out_unbatch = jraph.unbatch(out_graphs)
+
+    for i, in_graph in enumerate(jraph.unbatch(batch_graphs)):
+        out_graph = out_unbatch[i]
+        assert out_graph.n_node == in_graph.n_node
+
+
+def test_ew_loss_function(batch_graphs, mlp_kwargs):
+    max_num_nodes = jnp.max(batch_graphs.n_node) + 1
+    multi_edge_repeat = bag_gae.find_multi_edge_repeat(batch_graphs)
+
+    mlp_stack = [1, 4, 32, 16, 4, batch_graphs.nodes.shape[1]]
+    mpg_stack = [[2], [4, 8], [16], [2]]
+
+    model = edge_weight_gae.EdgeWeightGAE(
+        max_num_nodes=max_num_nodes,
+        encoder_stack=mpg_stack,
+        node_stack=mlp_stack,
+        edge_stack=mlp_stack,
+        decoder_mpg_stack=mpg_stack,
+        multi_edge_repeat=multi_edge_repeat,
+        mlp_kwargs=mlp_kwargs,
+    )
+
+    rng = jax.random.key(72)
+    rng, rng_split_a, rng_split_b = jax.random.split(rng, 3)
+
+    params = model.init({"params": rng, "reparametrize": rng_split_a, "dropout": rng_split_b}, batch_graphs)
+    rng, rng_split_a, rng_split_b = jax.random.split(rng, 3)
+    tx = optax.adam(learning_rate=1e-3)
+    opt_state = tx.init(params)
+    train_state = TrainState(step=0, apply_fn=model.apply, params=params, tx=tx, opt_state=opt_state)
+    train_state.apply_fn(train_state.params, batch_graphs, rngs={"reparametrize": rng_split_a, "dropout": rng_split_b})
+
+    metric_model = mpg_edge_weight.MessagePassingEW(
+        node_feature_sizes=mpg_stack,
+        edge_feature_sizes=mpg_stack,
+        global_feature_sizes=mpg_stack,
+        mean_instead_of_sum=False,
+    )
+    rng, rng_split = jax.random.split(rng)
+    metric_params = metric_model.init(rng_split, batch_graphs)
+    metric_state = TrainState(apply_fn=metric_model.apply, params=metric_params, step=None, tx=None, opt_state=None)
+
+    partial_step = partial(edge_weight_gae.train_step, metric_state=metric_state)
+    func = jax.jit(partial_step)
+
+    for _ in range(30):
+        train_state, train_loss, train_recon, train_kl, train_sharp, train_n_edge, test_recon, test_kl, test_sharp, test_n_edge = func(batch_graphs, batch_graphs, train_state, rng=rng_split)
+        rng, rng_split = jax.random.split(rng)
+
+        print(train_loss, train_recon, train_kl, train_sharp, train_n_edge, test_recon, test_kl, test_sharp, test_n_edge)
